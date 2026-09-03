5f1e6d15e1838b542e6ca3b096cfac19b02e906f
Mark <marknikiforesku@gmail.com>
2026-09-03 10:35:58 +0300

    compile typescript

--- server/dist/index.js
+++ server/dist/index.js
@@ -7,9 +7,11 @@ const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
+const PORT = process.env.PORT || 3000;
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, cors_1.default)());
+console.log("Images path:", path_1.default.join(__dirname, "../public/images"));
 app.use("/images", express_1.default.static(path_1.default.join(__dirname, "../public/images")));
 app.get("/", (req, res) => {
     console.log(path_1.default.join(__dirname, "../public"));
@@ -18,48 +20,49 @@ app.get("/", (req, res) => {
             name: "Boilded Egg",
             price: 10,
             text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-            image: "/images/egg.png",
+            image: "https://foody-zone-qmza.onrender.com/images/egg.png",
             type: "breakfast",
+            test: "RENDER_NEW_CODE_123"
         },
         {
             name: "RAMEN",
             price: 25,
             text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-            image: "/images/ramen.png",
+            image: "https://foody-zone-qmza.onrender.com/images/ramen.png",
             type: "lunch",
         },
         {
             name: "GRILLED CHICKEN",
             price: 45,
             text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-            image: "/images/chicken.png",
+            image: "https://foody-zone-qmza.onrender.com/images/chicken.png",
             type: "dinner",
         },
         {
             name: "CAKE",
             price: 18,
             text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-            image: "/images/cake.png",
+            image: "https://foody-zone-qmza.onrender.com/images/cake.png",
             type: "breakfast",
         },
         {
             name: "BURGER",
             price: 23,
             text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-            image: "/images/burger.png",
+            image: "https://foody-zone-qmza.onrender.com/images/burger.png",
             type: "lunch",
         },
         {
             name: "PANCAKE",
             price: 25,
             text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-            image: "/images/pancake.png",
+            image: "https://foody-zone-qmza.onrender.com/images/pancake.png",
             type: "dinner",
         },
     ];
     res.json(foodData);
 });
-app.listen(9000, () => {
-    console.log("Server is running on port 9000");
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
 //# sourceMappingURL=index.js.map
